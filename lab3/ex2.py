c008a855d6694570166fd7e5203af58ed0b99d48
Soul Sneezer <petrerobertcristian@gmail.com>
2025-11-03 20:54:27 +0200

    Finished ex2

--- lab3/ex2.py
+++ lab3/ex2.py
@@ -4,8 +4,9 @@ from common import SignalType
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from matplotlib.animation import FuncAnimation, PillowWriter
 
-fig, axes = plt.subplots(2, 1, figsize=(10, 12))
+fig, axes = plt.subplots(5, 1, figsize=(10, 12))
 precision = 0.00001
 duration = 0.1
 sine_signal = Signal("Example Signal", 1, 80, 0, SignalType.SINE, duration, precision).with_sampling(1000)
@@ -14,13 +15,42 @@ print(sine_signal.samples)
 
 length = len(sine_signal.get_function())
 
-y = sine_signal.get_function() * math.e ** (-2 * np.pi * 1j * np.arange(0, 1, 1 / length))
-print(y)
+omega_values = [1, 2, 5, 7]
+y_values = [0, 0, 0, 0]
 sine_signal.plot_signal(axes[0])
-axes[1].plot(y.real, y.imag)
-axes[1].set_xlim(-1, 1)
-axes[1].set_ylim(-1, 1)
-plt.gca().set_aspect('equal', adjustable='box')
+
+for i in range(len(omega_values)):
+    axes[1 + i].set_aspect('equal', adjustable='box')
+    y_values[i] = sine_signal.get_function() * math.e ** (-2 * np.pi * 1j * omega_values[i] * np.arange(0, 1, 1 / length))
+    axes[1 + i].plot(y_values[i].real, y_values[i].imag)
+    axes[1 + i].set_xlim(-1, 1)
+    axes[1 + i].set_ylim(-1, 1)
+
+
 plt.grid()
 plt.show()
 
+x = y_values[i].real
+y = y_values[i].imag
+
+fig, ax = plt.subplots(figsize=(5, 5))
+ax.set_xlim(-1, 1)
+ax.set_ylim(-1, 1)
+ax.set_aspect('equal', adjustable='box')
+ax.grid(True)
+
+line, = ax.plot([], [], linewidth=2)
+point, = ax.plot([], [], 'o')
+
+def update(i):
+    line.set_data(x[:i], y[:i])
+    point.set_data([x[i]], [y[i]])
+    return line, point
+
+frames = len(x)
+anim = FuncAnimation(fig, update, frames=frames, interval=1, blit=False)
+
+writer = PillowWriter(fps=60)
+anim.save("signal.gif", writer=writer)
+
+plt.show()
