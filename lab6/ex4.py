c67af62c5bbf4f7052a452c14a5f99e4d90285d6
Robert Cristian Petre <robert.petre@Roberts-MacBook-Pro.local>
2025-11-18 16:50:46 +0200

    Fixed ex4

--- lab6/ex4.py
+++ lab6/ex4.py
@@ -1,19 +1,22 @@
 import numpy as np  
 import scipy
 
-#x = np.random.randint(low=0, high=100, size=20)
-t = np.arange(20)
-x = np.sin(2*np.pi*t/20)
+x = np.random.randint(low=0, high=100, size=20)
+#t = np.arange(20)
+#x = np.sin(2*np.pi*t/20)
 
 d = 5
 
 y = np.roll(x, d)
 
-d2 = scipy.fft.ifft(scipy.fft.fft(x) * scipy.fft.fft(y))
+d2 = scipy.fft.ifft(np.conj(scipy.fft.fft(x)) * scipy.fft.fft(y))
 d3 = scipy.fft.ifft(scipy.fft.fft(y) / scipy.fft.fft(x))
 
 print(d2)
 print(d3)
 
-idx = np.argmax(d2.real)
-print(idx)
+index = np.argmax(d2.real)
+print(index)
+
+index = np.argmax(np.abs(d3))
+print(index)
