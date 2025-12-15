c4b0a9e3f316f64d63be37034d18e687823d0bd1
Soul Sneezer <petrerobertcristian@gmail.com>
2025-12-15 21:58:07 +0200

    Finished ex1 c

--- lab8/ex1.py
+++ lab8/ex1.py
@@ -11,6 +11,7 @@ X = np.linspace(0, 10, N)
 trend = X ** 2 - 4 * X + 7
 seasonal = np.sin(2 * np.pi * X * 5) + np.sin(2 * np.pi * X * 30)
 residuals = np.random.normal(0, 2, size=N)
+observed = trend + seasonal + residuals
 
 fig = plt.figure(figsize=(12, 8))
 
@@ -27,7 +28,47 @@ ax3.plot(X, residuals)
 ax3.set_title("Residuals")
 
 ax4 = fig.add_subplot(224)
-ax4.plot(X, trend + seasonal + residuals)
+ax4.plot(X, observed)
 ax4.set_title("Observed")
+
+plt.show()
+
+# 1 b
+
+observed_centered = observed - np.mean(observed)
+correlation = np.correlate(observed_centered, observed_centered, "full")
+correlation = correlation[correlation.size // 2:] / correlation[0] 
+plt.plot(X, correlation)
+plt.title("Correlation")
+plt.show()
+
+# 1 c
+
+def fit_ar_numpy(data, p):
+    n = len(data)
+    X = []
+    Y = []
+    
+    for i in range(p, n):
+        past_window = data[i-p : i][::-1]
+        row = np.insert(past_window, 0, 1.0)
+        Y.append(row)
+        X.append(data[i])
+        
+    X = np.array(X)
+    Y = np.array(Y)
+    
+    beta, residuals, rank, s = np.linalg.lstsq(Y, X)
+    predictions = Y.dot(beta)
+    
+    return beta, predictions
+
+p = 100
+coefficients, predictions = fit_ar_numpy(observed, p)
+
+plt.figure(figsize=(12, 6))
+plt.plot(X, observed, label='Original', color='gray')
+plt.plot(X[p:], predictions, label=f'AR({p}) Prediction', color='blue')
+plt.title("AR Model for Time Series")
 
 plt.show()
