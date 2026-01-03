46971967c1c45b68a4abcfbd0dea4d0cc12ac408
Soul Sneezer <petrerobertcristian@gmail.com>
2026-01-03 03:48:10 +0200

    Flattening DCT + Quantization tables

--- jpeg/jpeg.py
+++ jpeg/jpeg.py
@@ -6,6 +6,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fft import dctn, idctn
 
+# Standard Quantization Table for Y
+Q_LUMA = np.array([
+    [16, 11, 10, 16, 24, 40, 51, 61],
+    [12, 12, 14, 19, 26, 58, 60, 55],
+    [14, 13, 16, 24, 40, 57, 69, 56],
+    [14, 17, 22, 29, 51, 87, 80, 62],
+    [18, 22, 37, 56, 68, 109, 103, 77],
+    [24, 35, 55, 64, 81, 104, 113, 92],
+    [49, 64, 78, 87, 103, 121, 120, 101],
+    [72, 92, 95, 98, 112, 100, 103, 99]
+])
+
+# Standard Quantization Table for Chrominence
+Q_CHROMA = np.array([
+    [17, 18, 24, 47, 99, 99, 99, 99],
+    [18, 21, 26, 66, 99, 99, 99, 99],
+    [24, 26, 56, 99, 99, 99, 99, 99],
+    [47, 66, 99, 99, 99, 99, 99, 99],
+    [99, 99, 99, 99, 99, 99, 99, 99],
+    [99, 99, 99, 99, 99, 99, 99, 99],
+    [99, 99, 99, 99, 99, 99, 99, 99],
+    [99, 99, 99, 99, 99, 99, 99, 99]
+])
+
 # handle command line arguments
 parser = argparse.ArgumentParser(description="A script that compresses RGB images using jpeg")
 
@@ -55,16 +79,16 @@ Y = np.clip(0.299 * R + 0.587 * G + 0.114 * B, 0, 255)
 Cb = np.clip(-0.16874 * R - 0.33126 * G + 0.5 * B + 128, 0, 255)
 Cr = np.clip(0.5 * R - 0.41869 * G - 0.08131 * B + 128, 0, 255)
 
-fig, axes = plt.subplots(2, 2, figsize=(20, 5))
-axes = axes.flatten()
+# fig, axes = plt.subplots(2, 2, figsize=(20, 5))
+# axes = axes.flatten()
 
-axes[0].imshow(image_data)
-axes[0].set_title("Original RGB")
-axes[0].axis('off')
+# axes[0].imshow(image_data)
+# axes[0].set_title("Original RGB")
+# axes[0].axis('off')
 
-axes[1].imshow(Y, cmap='gray')
-axes[1].set_title("Y (Brightness)")
-axes[1].axis('off')
+# axes[1].imshow(Y, cmap='gray')
+# axes[1].set_title("Y (Brightness)")
+# axes[1].axis('off')
 
 # compress CbCr channels (4x)
 # also called chroma subsampling
@@ -73,16 +97,16 @@ axes[1].axis('off')
 Cb_compressed = Cb[::2, ::2]
 Cr_compressed = Cr[::2, ::2]
 
-axes[2].imshow(Cb_compressed, cmap='gray')
-axes[2].set_title("Cb (Blue Chroma)")
-axes[2].axis('off')
+# axes[2].imshow(Cb_compressed, cmap='gray')
+# axes[2].set_title("Cb (Blue Chroma)")
+# axes[2].axis('off')
 
-axes[3].imshow(Cr_compressed, cmap='gray')
-axes[3].set_title("Cr (Red Chroma)")
-axes[3].axis('off')
+# axes[3].imshow(Cr_compressed, cmap='gray')
+# axes[3].set_title("Cr (Red Chroma)")
+# axes[3].axis('off')
 
-plt.tight_layout()
-plt.show()
+# plt.tight_layout()
+# plt.show()
 
 # split image into 8x8 blocks and then apply DCT on these blocks
 # by 'image' I mean all channels: Y, Cb, Cr
@@ -107,11 +131,37 @@ def apply_dct_to_channel(channel):
     return dct_blocks
 
 dct_Y = apply_dct_to_channel(Y)
-dct_Cb = apply_dct_to_channel(compressed_Cb)
-dct_Cr = apply_dct_to_channel(compressed_Cr)
+dct_Cb = apply_dct_to_channel(Cb_compressed)
+dct_Cr = apply_dct_to_channel(Cr_compressed)
 
 # take the results of DCT in the form of a matrix and make it sparser
 # based on a quality component
+
+def do_the_zig_zag(block):
+    flipped = np.flipud(block)
+
+    return np.concatenate([
+        np.diagonal(flipped, offset=i)[::1 if i % 2 == 0 else -1]
+        for i in range(-7, 8)
+    ])
+
+def flatten_blocks(quantized_channel):
+    h, w = quantized_channel.shape
+    flattened_blocks = []
+
+    for i in range(0, h, 8):
+        for j in range(0, w, 8):
+            block = quantized_channel[i:i+8, j:j+8]
+            
+            flat_block = do_the_zig_zag(block)
+            
+            flattened_blocks.append(flat_block)
+            
+    return flattened_blocks
+
+flattened_dct_Y = flatten_blocks(dct_Y)
+flattened_dct_Cb = flatten_blocks(dct_Cb)
+flattened_dct_Cr = flatten_blocks(dct_Cr)
 
 # do run-length encoding in a zig-zag on the new matrix
 
